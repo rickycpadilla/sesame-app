7969003b8d614765ce89565c6e23166d38171faf
Ricky Padilla <rickycpadilla@gmail.com>
2016-09-06 10:40:34 -0600

    Added working arduino code, firebase, and johnny-five packages

--- arduino-code/servo-continuous.js
+++ arduino-code/servo-continuous.js
@@ -1,70 +1,33 @@
 var five = require("johnny-five");
 var board = new five.Board();
-
-board.on("ready", function() {
-  var servo = new five.Servo({
-    pin: 10,
-    range: [35, 145],
-    startAt: 35
-  }
-  );
-
-  // Servo alternate constructor with options
-  /*
-  var servo = new five.Servo({
-    id: "MyServo",     // User defined id
-    pin: 10,           // Which pin is it attached to?
-    type: "standard",  // Default: "standard". Use "continuous" for continuous rotation servos
-    range: [0,180],    // Default: 0-180
-    fps: 100,          // Used to calculate rate of movement between positions
-    invert: false,     // Invert all specified positions
-    startAt: 90,       // Immediately move to a degree
-    center: true,      // overrides startAt if true and moves the servo to the center of the range
-    specs: {           // Is it running at 5V or 3.3V?
-      speed: five.Servo.Continuous.speeds["@5.0V"]
-    }
-  });
-  */
-
-  // Add servo to REPL (optional)
-  this.repl.inject({
-    servo: servo
-  });
+var firebase = require("firebase");
+var config = {
+  databaseURL: "https://todo-cc9d9.firebaseio.com/",
+  storageBucket: "",
+};
 
 
-  // Servo API
+firebase.initializeApp(config);
+ var database = firebase.database();
+ var items = firebase.database().ref('items');
 
-  // min()
-  //
-  // set the servo to the minimum degrees
-  // defaults to 0
-  //
-  // eg. servo.min();
+ board.on("ready", function() {
+   var servo = new five.Servo({
+     pin: 10,
+     range: [35, 145],
+     startAt: 35
+   }
+   );
 
-  // max()
-  //
-  // set the servo to the maximum degrees
-  // defaults to 180
-  //
-  // eg. servo.max();
+   items.on('value', function(snapshot) {
+     var arr = Object.keys(snapshot.val())
+     var command = snapshot.val()[arr[arr.length - 1]].locked
+     console.log(command);
+     if (command == true) {
+          servo.min();
+     } else {
+          servo.max()
+     }
+   });
 
-  // center()
-  //
-  // centers the servo to 90°
-  //
-  // servo.center();
-
-  // to( deg )
-  //
-  // Moves the servo to position by degrees
-  //
-  // servo.to( 90 );
-
-  // step( deg )
-  //
-  // step all servos by deg
-  //
-  // eg. array.step( -20 );
-
-  servo.sweep();
-});
+ });
